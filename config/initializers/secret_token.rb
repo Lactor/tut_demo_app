56bfe698f97636c71f565bfc239d004775365bcb
Lactor <machado007@gmail.com>
2013-12-28 09:47:23 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TutDemoApp::Application.config.secret_key_base = '063e13826d4ceb1a443d108f691c0ce58143849d704bcd21964dfd5a455a6b0ebafd116b7ae71667db4562766f2fe346be1a81ee8efe5104a3de3061c9155a24'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+TutDemoApp::Application.config.session_store :cookie_store, key: '_tut_demo_app_session'
